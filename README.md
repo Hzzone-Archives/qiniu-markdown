7e74a489da97fe7925e06e7d9c5683d129d7c6f0
Hzzone <Hzzone@outlook.com>
2017-08-16 21:30:29 +0800

    upload image

---
+++ README.md
@@ -0,0 +1,17 @@
+参考[七牛python SDK](https://developer.qiniu.com/kodo/sdk/1242/python)  
+在```config.json```中配置```ak, sk, bucket name, bucket domain```       
+如何使用:
+```md.py file_path -o test.txt -r input_dir```  
+具体可以通过以下查看:
+```
+md.py -h
+```
+```
+md.py --help
+```
+首先安装```pip install qiniu```     
+
+为了能够实现复制粘贴功能,需要安装```pyperclip```
+```
+sudo pip3 install pyperclip
+```
---
+++ md.py
@@ -0,0 +1,74 @@
+import qiniu
+import json
+import os
+import getopt
+import sys
+
+# init the config and auth
+def init():
+    cfg = config()
+    global access_key
+    global secret_key
+    global bucket_name
+    global domain
+    global q
+    access_key = cfg["access_key"]
+    secret_key = cfg["secret_key"]
+    bucket_name = cfg["bucket_name"]
+    domain = cfg["domain"]
+    # 构建鉴权对象
+    q = qiniu.Auth(access_key, secret_key)
+
+def config():
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), "config.json")):
+        print("Config File Not Found in Current Folder")
+    f = open("config.json", encoding='utf-8')
+    cfg = json.load(f)
+    return cfg
+
+# upload and return markdown link
+def upload(path):
+    if not os.path.exists(path):
+        print("%s not exists" % path)
+
+    file_name = os.path.basename(path)
+    key = None
+    # print(key)
+    token = q.upload_token(bucket_name, key, 3600)
+    # 要上传文件的本地路径
+    ret, info = qiniu.put_file(token, key, path)
+    # print(info["text_body"]["key"])
+    # print(ret['key'])
+    link = "http://"+domain+"/"+ret["key"]
+    link = "![](%s)" % link
+    # print(link)
+    return file_name, link
+    # assert ret['key'] == key
+    # assert ret['hash'] == qiniu.etag(file_name)
+
+def help():
+    print("How to Use")
+
+if __name__ == "__main__":
+    init()
+    # upload("test.png")
+    try:
+        options, args = getopt.getopt(sys.argv[1:], "hp:i:", ["help", "ip=", "port="])
+    except getopt.GetoptError:
+        sys.exit()
+
+    if len(args)>1:
+        print("Param is no more than 1")
+        exit(0)
+    # upload one image
+    path = args[0]
+    print(upload(path))
+    for name, value in options:
+        if name in ("-h", "--help"):
+            help()
+        if name in ("-i", "--ip"):
+            pass
+        if name in ("-p", "--port"):
+            print('port is----', value)
+    options, args = getopt.getopt(sys.argv[1:], "hp:i:", ["help", "ip=", "port="])
+
